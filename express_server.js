f3185b66f84867d50e3658c416f470a2769f3d88
Ankit Anand <akanand.ca@gmail.com>
2020-08-06 01:55:59 +0000

    final

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
+
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+app.set("view engine", "ejs")
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,11 +21,22 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-//  });
+app.get("/hello", (req, res) => {
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
  
-//  app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-//  });
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+
+ app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
